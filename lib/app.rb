b7a8b571765dd3e63cbfbfb7015c34259eb7eeb4
Luke Phyall <lukeprospect@yahoo.com>
2019-02-18 15:21:31 +0000

    Adds shotgun gem

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
 
 get '/' do
   "hello!"
+end
+
+get '/secret' do
+  "secret"
+end
+
+get '/wednesday' do
+  "wednesday, just for you"
+end
+
+get '/schofield' do
+  "88"
 end
